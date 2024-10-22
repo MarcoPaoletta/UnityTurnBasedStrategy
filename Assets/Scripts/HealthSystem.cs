556951d28a64f3e40a8c0c9ec730b815b7ec03b4
Marco Paoletta <84252721+MarcoPaoletta@users.noreply.github.com>
2024-10-22 13:50:01 -0300

    Unit world UI

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,8 +6,16 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
 
     [SerializeField] private int health = 100;
+
+    private int healthMax;
+
+    private void Awake()
+    {
+        healthMax = health;
+    }
 
     public void Damage(int damageAmount)
     {
@@ -18,15 +26,21 @@ public class HealthSystem : MonoBehaviour
             health = 0;
         }
 
+        OnDamaged?.Invoke(this, EventArgs.Empty);
+
         if (health == 0)
         {
             Die();
         }
-
     }
 
     private void Die()
     {
         OnDead?.Invoke(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / healthMax;
     }
 }
