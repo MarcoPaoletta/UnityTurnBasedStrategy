673c286a965b6b5f7e7e780ddbf54259964598ba
Marco Paoletta <84252721+MarcoPaoletta@users.noreply.github.com>
2024-11-03 20:09:44 -0300

    Interface IInteractable

--- Assets/Scripts/Actions/InteractAction.cs
+++ Assets/Scripts/Actions/InteractAction.cs
@@ -46,8 +46,8 @@ public class InteractAction : BaseAction
                     continue;
                 }
 
-                Door door = LevelGrid.Instance.GetDoorAtGridPosition(testGridPosition);
-                if (door == null)
+                IInteractable interactable = LevelGrid.Instance.GetInteractableAtGridPosition(testGridPosition);
+                if (interactable == null)
                 {
                     continue;
                 }
@@ -61,8 +61,8 @@ public class InteractAction : BaseAction
 
     public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
     {
-        Door door = LevelGrid.Instance.GetDoorAtGridPosition(gridPosition);
-        door.Interact(OnInteractComplete);
+        IInteractable interactable = LevelGrid.Instance.GetInteractableAtGridPosition(gridPosition);
+        interactable.Interact(OnInteractComplete);
 
         ActionStart(onActionComplete);
     }
